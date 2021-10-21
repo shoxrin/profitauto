9433b310fe08c94f919cc6896166588bad38f610
shoxrin <kshoxrin@gmail.com>
2021-10-21 14:19:32 +0300

    test

--- auto.ru/monitor1/config.py
+++ auto.ru/monitor1/config.py
@@ -75,7 +75,10 @@ PARAMS = {
 
 WEBHOOK_URLS = {
     'СПБ': {
-        'до 300к': 'https://discord.com/api/webhooks/900166495085146122/Io8Qg_UsZzKRlee7nyfpRMA3BwOWnYYX3Nw1lubFJGWjN9AiSmQY_54Du8OdybI7sV4x'
+        'до 300к': 'https://discord.com/api/webhooks/900166495085146122/Io8Qg_UsZzKRlee7nyfpRMA3BwOWnYYX3Nw1lubFJGWjN9AiSmQY_54Du8OdybI7sV4x',
+        '300-700': 'https://discord.com/api/webhooks/900704728353214466/NWgixfk3_nd6LSzaUV00K2E4IqckqGCdAeSmyoa013gQIZH7VcIgOyBCQzVzQC4jra8L',
+        '700-1,5млн': 'https://discord.com/api/webhooks/900704641820545054/wcPWP8uAqsTAUvJt_v1jKBV_Z2CfU7hVrbpAClnjkh9WjUN_BQkIaT7ERNx9qLS1ymqL',
+        '1,5млн>': 'https://discord.com/api/webhooks/900704451017469993/KR0VJxQrskaWJsEzRaEp2kYBQSJI5O7LC7R3vfrUkVrkwBcbJUWs0aawksZCI2tjaFIk'
         #'до 300к': 'https://discord.com/api/webhooks/895295379522850897/aSXGnNh0R9dPVZ0BgI-M4t2Q021XB3h5iR2W2tfc2NGNm1nmwJFRniQxgJj-Tl2IjdDj',
         #'300-700': 'https://discord.com/api/webhooks/895294828257083442/Z7ZfJN_wGTG6ArxioAfbddqKZYYQpdWG_a9eiR3QuVBBjvKSamxf53lcD-Wxbj85sdT5',
         #'700-1,5млн': 'https://discord.com/api/webhooks/895294741778956340/c-ff-wHIcYNBO4z1Ouc56kTfTjPEx7wJ-DBXKlu8AyaZPMWzbCmC1ACSVoylbOBaGlK1',
