60f5af5992cbd92b50504e5a81618d8ce54ac50c
shoxrin <kshoxrin@gmail.com>
2021-10-31 02:51:04 +0300

    test

---
+++ drom.ru/monitor1/main.py
@@ -0,0 +1,104 @@
+import time
+import logging
+
+import discord
+from config import URLS, WEBHOOK_URLS
+import parser
+from discord import Webhook, RequestsWebhookAdapter, Embed, Colour
+
+
+class Monitor:
+    def __init__(self, urls, webhook_urls):
+        self.logger = logging.getLogger(__name__) #логер
+        self.get_logger() #Вызов функции для настройки логгера
+        self.urls = urls #Список url для запросов
+        self.webhook_urls = webhook_urls #Список вебхуков
+        self.parser = parser.Parser() #Создание объекта парсера
+    
+    #Функция запуска монитора
+    def run(self):
+        while True:
+            try:
+                #Перебор регионов
+                for geo in self.urls:
+                    #Перебор url запросов
+                    for url in self.urls[geo]:
+                        self.logger.info('Поиск новых объявлений! %s', str(geo) + ', ' + str(url))
+                        #Список объявлений
+                        announcements = self.parser.getAnnouncements(self.urls[geo][url])
+                        #Если есть объявления
+                        if announcements:
+                            #Перебор объявлений
+                            for announcement in announcements:
+                                self.logger.info('Отправка - %s', announcement['title'] + ', ' + announcement['time'])
+                                #Отпрака объявления в дискорд
+                                mesinfo = self.sendMessage(announcement, self.webhook_urls[geo][url])
+                                #Если объявление не отпраленно
+                                if not(mesinfo):
+                                    time.sleep(3)
+                                    self.logger.info('Повторная отправка - %s', announcement['title'] + ', ' + announcement['time'])
+                                    #Повторная отправка
+                                    mesinfo = self.sendMessage(announcement, self.webhook_urls[geo][url])
+                                time.sleep(1)
+                        #Если нет объявлений
+                        else:
+                            self.logger.error('Новых объявлений нет!')
+                        time.sleep(10)
+                #Задержка перед следуюшим регионом
+                time.sleep(8)
+            except Exception as ex:
+                self.logger.error('Ошибка! %s', ex)
+                #Вслучае ошибки
+                time.sleep(5)
+
+    #Отправка объявления в канал
+    def sendMessage(self, announcement, webhook_url):
+        #Создание вебхука
+        webhook = Webhook.from_url(webhook_url, adapter=RequestsWebhookAdapter()) 
+        #Создание вставки сообщения
+        embed = Embed(
+                    color = Colour.blue(), 
+                    title = announcement['title']
+                )
+        try:
+            embed.set_thumbnail(url = announcement['img']['src'])
+            embed.add_field(name = 'Цена', value = announcement['price'])
+            embed.add_field(name = 'Параметры', value = announcement['params'])
+            embed.add_field(name = 'Местоположение', value = announcement['geo'])
+            embed.add_field(name = 'Ссылка', value = announcement['link'])
+            webhook.send(embed=embed)
+            self.logger.info('Отправлено - %s', announcement['title'])
+
+            return True
+        except TypeError:
+            embed.add_field(name = 'Цена', value = announcement['price'])
+            embed.add_field(name = 'Параметры', value = announcement['params'])
+            embed.add_field(name = 'Местоположение', value = announcement['geo'])
+            embed.add_field(name = 'Ссылка', value = announcement['link'])
+            webhook.send(embed=embed)
+            self.logger.info('Отправлено - %s', announcement['title'])
+            
+            return True
+        except:
+            self.logger.info('Ошибка отправки! %s', announcement['title'])
+            return False
+
+    #Создание логгера
+    def get_logger(self):
+        self.logger.setLevel(logging.INFO)
+
+        c_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler('../logs/monitor1.log')
+        c_handler.setLevel(logging.INFO)
+        f_handler.setLevel(logging.INFO)
+
+        c_format = logging.Formatter('[%(asctime)s][%(levelname)s]:%(message)s', datefmt='%H:%M:%S')
+        f_format = logging.Formatter('[%(asctime)s][%(levelname)s]:%(message)s', datefmt='%H:%M:%S')
+        c_handler.setFormatter(c_format)
+        f_handler.setFormatter(f_format)
+
+        self.logger.addHandler(c_handler)
+        self.logger.addHandler(f_handler)
+
+if __name__ == '__main__':
+    Monitor(URLS, WEBHOOK_URLS).run()
---
+++ drom.ru/monitor1/parser.py
@@ -0,0 +1,89 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+
+
+class Parser:
+    def __init__(self):
+        self.tmp = [] #Временное хранилище ссылок для отправленных объявлений
+        self.session = requests.Session() #Создание сессии
+        #Заголовки 
+        self.headers = {
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-encoding": "gzip, deflate",
+            "accept-language": "en-US,en;q=0.9,fr-DZ;q=0.8,fr;q=0.7",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+            "x-requested-with": "XMLHttpRequest"
+        }
+        #Прокси
+        self.proxies = {
+            'https': 'http://user65270:03kyol@185.120.76.193:5609',
+            'http': 'http://user65270:03kyol@185.120.76.193:5609'
+        }
+        #Добавление прокси и заголовков к сессии
+        self.session.proxies = self.proxies
+        self.session.headers = self.headers
+
+    #Функция для получения объявлений
+    def getHTML(self, url):
+        response = self.session.get(url=url) #Создание запроса к сайту
+        soup = BeautifulSoup(response.content, 'html.parser')
+        items = soup.find_all('a', attrs={'data-ftid': 'bulls-list_bull'})
+        print('[STATUS_CODE]:', response.status_code)
+        while response.status_code == 429:
+            time.sleep(600)
+            response = self.session.get(url=url) #Создание запроса к сайту
+            soup = BeautifulSoup(response.content, 'html.parser') #Парсинг ответа
+            items = soup.find_all('a', attrs={'data-ftid': 'bulls-list_bull'}) #Формирование списка всех полученных объявлений
+            print('[STATUS_CODE]:', response.status_code)
+        return items
+
+    #Функция для сортировки объявлений и получении нужной информации
+    def getAnnouncements(self, url):
+        items = self.getHTML(url)
+        announcements = [] #Список с объявлениями
+        #Перебор html блоков объявлений для дальнейшей обработки
+        for item in items:
+            link = item.get('href') #Ссылка объявления
+            timeadd = item.find('div', attrs={'data-ftid': 'bull_date'}).text #Время создания объявления
+           #Проверка для получения новых объявлений
+            # or '2 минуты назад' == timeadd
+            if (timeadd == '1 минуту назад' or timeadd == 'Несколько секунд назад') and not(link in self.tmp):
+                self.tmp.append(link) #Добавление использованной ссылки объявления
+                #Наполнение списка с новыми объявлениями
+                announcements.append({
+                    'title': item.find('div', class_='css-1svsmzw e1vivdbi2').find('span').text,
+                    'price': item.find('span', class_="css-bhd4b0 e162wx9x0").text,
+                    'params': item.find('div', class_='css-3xai0o e162wx9x0').text,
+                    'geo': item.find('span', class_="css-fbscyn e162wx9x0").text.split()[0],
+                    'img': item.find('div', attrs={'data-ftid': 'bull_image'}).find('noscript').find('img'),
+                    'link': link,
+                    'time': timeadd 
+                })
+                #Очистка хранилища использованных ссылок
+                if len(self.tmp) == 30:
+                    del self.tmp[0:14]
+            else:
+                break
+
+        return announcements
+
+
+#p = Parser()
+#
+#while True:
+#    ann = p.getAnnouncements('https://auto.drom.ru/region78/?maxprice=300000')
+#    if ann:
+#        for an in ann:
+#            print(an['link'])
+#            print(an['title'])
+#            print(an['params'])
+#            time.sleep(2)
+#    else:
+#        print('No offers')
+#
+#    time.sleep(10)
