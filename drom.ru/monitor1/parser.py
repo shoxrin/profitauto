14d82758357a4c6c1fea84d330aeb2901ad549e3
shoxrin <kshoxrin@gmail.com>
2021-10-31 03:52:49 +0300

    test

--- drom.ru/monitor1/parser.py
+++ drom.ru/monitor1/parser.py
@@ -53,7 +53,7 @@ class Parser:
             print(timeadd)
             #Проверка для получения новых объявлений
             # or '2 минуты назад' == timeadd
-            if (timeadd == '1 минуту назад' or timeadd == 'Несколько секунд назад') and not(link in self.tmp):
+            if (timeadd == '1 минуту назад' or timeadd == 'Несколько секунд назад') and not(item.find('div', class_='css-x98spp e1vivdbi1')) and not(link in self.tmp):
                 self.tmp.append(link) #Добавление использованной ссылки объявления
                 #Наполнение списка с новыми объявлениями
                 announcements.append({
